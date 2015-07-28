6080b8337fd3f0d28cd430781fb7c92d18899903
Slitzweitz <colinmbarlow@gmail.com>
2015-07-28 14:38:01 -0700

    Site upload

---
+++ Scripts/hawkify.js
@@ -0,0 +1,24 @@
+﻿//Script to add/remove Chicago Blackhawks Feather colors to the page
+
+.gal {
+    padding: 5px 5px 5px 5px;
+    background: #1C418C;
+}
+#articles {
+    padding-top: 15px;
+    padding-bottom: 15px;
+    background: #E32739;
+    color: white;
+}
+.posts {
+    background: #007F55;
+}
+#image-wrap1 {
+    border: 2px solid #FEE011;
+}
+#image-wrap2 {
+    border: 2px solid #FEE011;
+}
+#low-img {
+    background: #FEE011;
+    border: 3px solid #1C418C;
